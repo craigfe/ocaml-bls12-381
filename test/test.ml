a3840635c441eacae9133d3417c2012c9bf422b6
Danny Willems <be.danny.willems@gmail.com>
2020-01-29 12:07:32 +0100

    Add some test properties

--- test/test.ml
+++ test/test.ml
@@ -10,6 +10,10 @@ module ValueGeneration = struct
     let random = Bls12_381.Fq.random () in
     if Bls12_381.Fq.is_zero random then inverse_with_random_not_null ()
     else ignore @@ Bls12_381.Fq.inverse random
+
+  let inverse_with_one () =
+    let one = Bls12_381.Fq.one () in
+    ignore @@ Bls12_381.Fq.inverse one
 end
 
 module IsZero = struct
@@ -57,6 +61,29 @@ module FieldProperties = struct
     let zero = Bls12_381.Fq.zero () in
     let one = Bls12_381.Fq.one () in
     assert (Bls12_381.Fq.is_zero (Bls12_381.Fq.mul zero one))
+
+  let inverse_of_one_is_one () =
+    let one = Bls12_381.Fq.one () in
+    assert (Bls12_381.Fq.eq (Bls12_381.Fq.inverse one) one)
+
+  let zero_has_no_inverse () =
+    let zero = Bls12_381.Fq.zero () in
+    match Bls12_381.Fq.inverse_opt zero with
+    | Some(_) -> assert(false)
+    | None -> assert(true)
+
+  let rec inverse_of_non_null_does_exist () =
+    let random = Bls12_381.Fq.random () in
+    if Bls12_381.Fq.is_zero random then inverse_of_non_null_does_exist ()
+    else match Bls12_381.Fq.inverse_opt random with
+    | Some(_) -> assert(true)
+    | None -> assert(false)
+
+  let rec inverse_of_inverse () =
+    let random = Bls12_381.Fq.random () in
+    if Bls12_381.Fq.is_zero random then inverse_of_inverse ()
+    else assert(Bls12_381.Fq.eq (Bls12_381.Fq.inverse (Bls12_381.Fq.inverse random)) random)
+
 end
 
 let () =
@@ -68,7 +95,9 @@ let () =
           test_case "with random value" `Quick IsZero.with_random_value ] );
       ( "value generation",
         [ test_case "zero" `Quick ValueGeneration.zero;
-          test_case "random" `Quick ValueGeneration.random ] );
+          test_case "random" `Quick ValueGeneration.random;
+          test_case "inverse_random_not_null" `Quick ValueGeneration.inverse_with_random_not_null;
+          test_case "inverse_one" `Quick ValueGeneration.inverse_with_one ] );
       ( "equality",
         [ test_case
             "zero_two_different_objects"
@@ -94,4 +123,22 @@ let () =
           test_case
             "zero_nullifier_random"
             `Quick
-            FieldProperties.zero_nullifier_random ] ) ]
+            FieldProperties.zero_nullifier_random;
+          test_case
+            "inverse_of_non_null_does_exist"
+            `Quick
+            FieldProperties.inverse_of_non_null_does_exist;
+          test_case
+            "inverse_of_one_is_one"
+            `Quick
+            FieldProperties.inverse_of_one_is_one;
+          test_case
+            "zero_has_no_inverse"
+            `Quick
+            FieldProperties.zero_has_no_inverse;
+          test_case
+            "inverse_of_inverse"
+            `Quick
+            FieldProperties.inverse_of_inverse;
+
+ ] ) ]
