951d65b8f2dc924822baffd8325e9d08636457a3
Danny Willems <be.danny.willems@gmail.com>
2020-01-29 11:42:26 +0100

    Lint mli + add lint in CI

--- src/ff_sig.mli
+++ src/ff_sig.mli
@@ -1,18 +1,29 @@
 (** General module signature for a finite field *)
 module type T = sig
   type t
+
   (* Let's use a function for the moment *)
   val zero : unit -> t
+
   val one : unit -> t
+
   val is_zero : t -> bool
+
   val is_one : t -> bool
+
   val random : unit -> t
+
   val add : t -> t -> t
+
   val mul : t -> t -> t
+
   val eq : t -> t -> bool
+
   val negate : t -> t
+
   (* Unsafe version of inverse *)
   val inverse : t -> t
+
   (* Safe version of inverse *)
   val inverse_opt : t -> t option
 end
