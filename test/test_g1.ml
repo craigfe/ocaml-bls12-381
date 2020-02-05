87711c459d9e39fee8fdc6e1af1056e13999a5c8
Danny Willems <be.danny.willems@gmail.com>
2020-02-05 16:04:37 +0100

    Add Compressed version for the tests

--- test/test_g1.ml
+++ test/test_g1.ml
@@ -5,12 +5,24 @@ module ValueGeneration =
 module IsZero = Test_ec_make.MakeIsZero (Bls12_381.G1.Uncompressed)
 module Equality = Test_ec_make.MakeEquality (Bls12_381.G1.Uncompressed)
 module ECProperties = Test_ec_make.MakeECProperties (Bls12_381.G1.Uncompressed)
+module ValueGenerationCompressed =
+  Test_ec_make.MakeValueGeneration (Bls12_381.G1.Compressed)
+module IsZeroCompressed = Test_ec_make.MakeIsZero (Bls12_381.G1.Compressed)
+module EqualityCompressed = Test_ec_make.MakeEquality (Bls12_381.G1.Compressed)
+module ECPropertiesCompressed =
+  Test_ec_make.MakeECProperties (Bls12_381.G1.Compressed)
 
 let () =
   let open Alcotest in
   run
-    "G1"
+    "G1 Uncompressed"
     [ IsZero.get_tests ();
       ValueGeneration.get_tests ();
       Equality.get_tests ();
-      ECProperties.get_tests () ]
+      ECProperties.get_tests () ] ;
+  run
+    "G1 Compressed"
+    [ IsZeroCompressed.get_tests ();
+      ValueGenerationCompressed.get_tests ();
+      EqualityCompressed.get_tests ();
+      ECPropertiesCompressed.get_tests () ]
--- test/test_g2.ml
+++ test/test_g2.ml
@@ -3,12 +3,24 @@ module ValueGeneration =
 module IsZero = Test_ec_make.MakeIsZero (Bls12_381.G2.Uncompressed)
 module Equality = Test_ec_make.MakeEquality (Bls12_381.G2.Uncompressed)
 module ECProperties = Test_ec_make.MakeECProperties (Bls12_381.G2.Uncompressed)
+module ValueGenerationCompressed =
+  Test_ec_make.MakeValueGeneration (Bls12_381.G2.Compressed)
+module IsZeroCompressed = Test_ec_make.MakeIsZero (Bls12_381.G2.Compressed)
+module EqualityCompressed = Test_ec_make.MakeEquality (Bls12_381.G2.Compressed)
+module ECPropertiesCompressed =
+  Test_ec_make.MakeECProperties (Bls12_381.G2.Compressed)
 
 let () =
   let open Alcotest in
   run
-    "G2"
+    "G2 uncompressed"
     [ IsZero.get_tests ();
       ValueGeneration.get_tests ();
       Equality.get_tests ();
-      ECProperties.get_tests () ]
+      ECProperties.get_tests () ] ;
+  run
+    "G2 compressed"
+    [ IsZeroCompressed.get_tests ();
+      ValueGenerationCompressed.get_tests ();
+      EqualityCompressed.get_tests ();
+      ECPropertiesCompressed.get_tests () ]
