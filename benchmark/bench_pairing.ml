077e34f2aa108d572759d6cab61654b094a8d1a9
Danny Willems <be.danny.willems@gmail.com>
2020-02-12 17:24:14 +0100

    Lint pairing benchmark file

--- benchmark/bench_pairing.ml
+++ benchmark/bench_pairing.ml
@@ -1,13 +1,15 @@
 open Core_bench
 
-
 let g1 = Bls12_381.G1.Uncompressed.random ()
+
 let g2 = Bls12_381.G2.Uncompressed.random ()
 
 let compute_pairing_on_pregenerated_random_element () =
   Bls12_381.Pairing.pairing g1 g2
 
 let () =
-  Core.Command.run (Bench.make_command [
-    Bench.Test.create ~name:"Pairing on pregenerated random element" compute_pairing_on_pregenerated_random_element
-  ])
+  Core.Command.run
+    (Bench.make_command
+       [ Bench.Test.create
+           ~name:"Pairing on pregenerated random element"
+           compute_pairing_on_pregenerated_random_element ])
