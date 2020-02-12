c0bd075dd202d58afcd4812c0c4c3a24c040afc5
Danny Willems <be.danny.willems@gmail.com>
2020-02-12 17:23:46 +0100

    More precise on the signature of the scalar module for G1 and G2

--- src/g2.mli
+++ src/g2.mli
@@ -1,7 +1,7 @@
 module Uncompressed : sig
-  include Elliptic_curve_sig.T
+  include Elliptic_curve_sig.T with type Scalar.t = Fr.t
 end
 
 module Compressed : sig
-  include Elliptic_curve_sig.T
+  include Elliptic_curve_sig.T with type Scalar.t = Fr.t
 end
