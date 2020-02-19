9df2ed1ae489e7e7223dee83062c9a90daf29fb0
Danny Willems <be.danny.willems@gmail.com>
2020-02-19 19:38:06 +0100

    Remove comments

--- src/pairing.ml
+++ src/pairing.ml
@@ -1,29 +1,3 @@
-(*
-module type T = sig
-  type g1
-
-  type g2
-
-  type g3
-
-  val miller_loop_simple : g1 -> g2 -> g3
-end
-
-module Make
-    (G1 : Elliptic_curve_sig.T)
-    (G2 : Elliptic_curve_sig.T)
-    (G3 : Elliptic_curve_sig.T) :
-  T with type g1 = G1.t and type g2 = G2.t and type g3 = G3.t = struct
-  type g1 = G1.t
-
-  type g2 = G2.t
-
-  type g3 = G3.t
-
-  let miller_loop_simple _g1 _g2 = G3.random ()
-end
-*)
-
 (** External definition, must use `Bytes.t` to represent the field elements *)
 external ml_bls12_381_pairing_miller_loop_simple :
   Bytes.t -> Bytes.t -> Bytes.t -> unit
