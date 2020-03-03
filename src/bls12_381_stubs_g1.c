d1e2dd2d5e62f6a233722b36fd1b8fb68475839a
Danny Willems <be.danny.willems@gmail.com>
2020-03-03 23:34:04 +0100

    Use correctly CAMLparam and CAMLreturn
    Revisit the C stubs

--- src/bls12_381_stubs_g1.c
+++ src/bls12_381_stubs_g1.c
@@ -9,52 +9,59 @@
 extern void rustc_bls12_381_g1_one(unsigned char *buffer);
 
 CAMLprim value ml_librustc_bls12_381_g1_one(value buffer) {
+  CAMLparam1(buffer);
   rustc_bls12_381_g1_one(Bytes_val(buffer));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
 
 extern void rustc_bls12_381_g1_zero(unsigned char *buffer);
 
 CAMLprim value ml_librustc_bls12_381_g1_zero(value buffer) {
+  CAMLparam1(buffer);
   rustc_bls12_381_g1_zero(Bytes_val(buffer));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
 
 extern void rustc_bls12_381_g1_random(unsigned char *element);
 
 CAMLprim value ml_librustc_bls12_381_g1_random(value element) {
+  CAMLparam1(element);
   rustc_bls12_381_g1_random(Bytes_val(element));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
 
 extern void rustc_bls12_381_g1_add(unsigned char *buffer, const unsigned char* g1, const unsigned char *g2);
 
 CAMLprim value ml_librustc_bls12_381_g1_add(value buffer, value g1, value g2) {
+  CAMLparam3(buffer, g1, g2);
   rustc_bls12_381_g1_add(Bytes_val(buffer), Bytes_val(g1), Bytes_val(g2));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
 
 extern void rustc_bls12_381_g1_negate(unsigned char *buffer,
                                       const unsigned char *g);
 
 CAMLprim value ml_librustc_bls12_381_g1_negate(value buffer, value g) {
+  CAMLparam2(buffer, g);
   rustc_bls12_381_g1_negate(Bytes_val(buffer), Bytes_val(g));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
 
 extern bool rustc_bls12_381_g1_eq(const unsigned char *g1,
-                                 const unsigned char *g2);
+                                  const unsigned char *g2);
 
 CAMLprim value ml_librustc_bls12_381_g1_eq(value g1, value g2) {
+  CAMLparam2(g1, g2);
   bool res = rustc_bls12_381_g1_eq(Bytes_val(g1), Bytes_val(g2));
-  return Val_bool(res);
+  CAMLreturn(Val_bool(res));
 }
 
 extern bool rustc_bls12_381_g1_is_zero(const unsigned char *g);
 
 CAMLprim value ml_librustc_bls12_381_g1_is_zero(value g) {
+  CAMLparam1(g);
   bool res = rustc_bls12_381_g1_is_zero(Bytes_val(g));
-  return Val_bool(res);
+  CAMLreturn(Val_bool(res));
 }
 
 extern void rustc_bls12_381_g1_mul(unsigned char *buffer,
@@ -62,47 +69,53 @@ extern void rustc_bls12_381_g1_mul(unsigned char *buffer,
                                   const unsigned char *a);
 
 CAMLprim value ml_librustc_bls12_381_g1_mul(value buffer, value g, value a) {
+  CAMLparam3(buffer, g, a);
   rustc_bls12_381_g1_mul(Bytes_val(buffer), Bytes_val(g), Bytes_val(a));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
 
 // ---------------- G1 Compressed ------------
 extern void rustc_bls12_381_g1_compressed_add(unsigned char *buffer, const unsigned char* g1, const unsigned char *g2);
 
 CAMLprim value ml_librustc_bls12_381_g1_compressed_add(value buffer, value g1, value g2) {
+  CAMLparam3(buffer, g1, g2);
   rustc_bls12_381_g1_compressed_add(Bytes_val(buffer), Bytes_val(g1), Bytes_val(g2));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
 
 extern void rustc_bls12_381_g1_compressed_negate(unsigned char *buffer,
                                                  const unsigned char *g);
 
 CAMLprim value ml_librustc_bls12_381_g1_compressed_negate(value buffer, value g) {
+  CAMLparam2(buffer, g);
   rustc_bls12_381_g1_compressed_negate(Bytes_val(buffer), Bytes_val(g));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
 
 extern bool rustc_bls12_381_g1_compressed_eq(const unsigned char *g1,
                                              const unsigned char *g2);
 
 CAMLprim value ml_librustc_bls12_381_g1_compressed_eq(value g1, value g2) {
+  CAMLparam2(g1, g2);
   bool res = rustc_bls12_381_g1_compressed_eq(Bytes_val(g1), Bytes_val(g2));
-  return Val_bool(res);
+  CAMLreturn(Val_bool(res));
 }
 
 extern bool rustc_bls12_381_g1_compressed_is_zero(const unsigned char *g);
 
 CAMLprim value ml_librustc_bls12_381_g1_compressed_is_zero(value g) {
+  CAMLparam1(g);
   bool res = rustc_bls12_381_g1_compressed_is_zero(Bytes_val(g));
-  return Val_bool(res);
+  CAMLreturn(Val_bool(res));
 }
 
 extern void rustc_bls12_381_g1_compressed_of_uncompressed(unsigned char *buffer,
                                                           const unsigned char *g);
 
 CAMLprim value ml_librustc_bls12_381_g1_compressed_of_uncompressed(value buffer, value g) {
+  CAMLparam2(buffer, g);
   rustc_bls12_381_g1_compressed_of_uncompressed(Bytes_val(buffer), Bytes_val(g));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
 
 extern void
@@ -110,31 +123,35 @@ rustc_bls12_381_g1_uncompressed_of_compressed(unsigned char *buffer,
                                               const unsigned char *g);
 
 CAMLprim value ml_librustc_bls12_381_g1_uncompressed_of_compressed(value buffer,
-                                                                   value g) {
+                                                         value g) {
+  CAMLparam2(buffer, g);
   rustc_bls12_381_g1_uncompressed_of_compressed(Bytes_val(buffer),
                                                 Bytes_val(g));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
 
 extern void rustc_bls12_381_g1_compressed_one(unsigned char *buffer);
 
 CAMLprim value ml_librustc_bls12_381_g1_compressed_one(value buffer) {
+  CAMLparam1(buffer);
   rustc_bls12_381_g1_compressed_one(Bytes_val(buffer));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
 
 extern void rustc_bls12_381_g1_compressed_zero(unsigned char *buffer);
 
 CAMLprim value ml_librustc_bls12_381_g1_compressed_zero(value buffer) {
+  CAMLparam1(buffer);
   rustc_bls12_381_g1_compressed_zero(Bytes_val(buffer));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
 
 extern void rustc_bls12_381_g1_compressed_random(unsigned char *element);
 
 CAMLprim value ml_librustc_bls12_381_g1_compressed_random(value element) {
+  CAMLparam1(element);
   rustc_bls12_381_g1_compressed_random(Bytes_val(element));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
 
 extern void rustc_bls12_381_g1_compressed_mul(unsigned char *buffer,
@@ -142,6 +159,7 @@ extern void rustc_bls12_381_g1_compressed_mul(unsigned char *buffer,
                                               const unsigned char *a);
 
 CAMLprim value ml_librustc_bls12_381_g1_compressed_mul(value buffer, value g, value a) {
+  CAMLparam3(buffer, g, a);
   rustc_bls12_381_g1_compressed_mul(Bytes_val(buffer), Bytes_val(g), Bytes_val(a));
-  return Val_unit;
+  CAMLreturn(Val_unit);
 }
