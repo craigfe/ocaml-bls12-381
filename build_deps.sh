0dd6799d0a659e44e9c46490e72cceb39ec5ca58
Danny Willems <be.danny.willems@gmail.com>
2020-06-02 19:37:27 +0200

    Change library finding

--- build_deps.sh
+++ build_deps.sh
@@ -11,25 +11,21 @@ function build_rustc_bls12_381 () {
   local commit="3c45daa1b4afbb8031d99b266866665d581bf421"
   local repository_name="rustc-bls12-381"
   local repository="https://gitlab.com/dannywillems/rustc-bls12-381"
-  local install_dir="${OPAM_SWITCH_PREFIX}/lib/librustc-bls12-381"
+  local install_dir="${OPAM_SWITCH_PREFIX}/lib"
   local full_url_zip="${repository}/-/archive/${commit}/${repository_name}-${commit}.zip"
 
-  if ! [ -d ${install_dir} ] ; then
-      echo "Installing rustc-bls12-381 in ${install_dir}"
-      command -v cargo > /dev/null 2>&1 || { echo >&2 "Install cargo. Aborting."; exit 1; }
-      mkdir -p ${BUILD_DIR}
-      cd ${BUILD_DIR}
-      echo "Downloading rustc-bls12-381 from ${full_url_zip}"
-      curl -L ${full_url_zip} --output ${commit}.zip
-      unzip ${commit}.zip >> /dev/null # do not print the resulting file
-      cd ${repository_name}-${commit}
-      export RUST_BACKTRACE=1
-      cargo build --release
-      rm -rf ${install_dir}
-      mkdir ${install_dir}
-      cp ${BUILD_DIR}/${repository_name}-${commit}/target/release/librustc_bls12_381.a ${install_dir}
-      rm -rf ${BUILD_DIR}
-  fi
+  echo "Installing rustc-bls12-381 in ${install_dir}"
+  command -v cargo > /dev/null 2>&1 || { echo >&2 "Install cargo. Aborting."; exit 1; }
+  mkdir -p ${BUILD_DIR}
+  cd ${BUILD_DIR}
+  echo "Downloading rustc-bls12-381 from ${full_url_zip}"
+  curl -L ${full_url_zip} --output ${commit}.zip
+  unzip ${commit}.zip >> /dev/null # do not print the resulting file
+  cd ${repository_name}-${commit}
+  export RUST_BACKTRACE=1
+  cargo build --release
+  cp ${BUILD_DIR}/${repository_name}-${commit}/target/release/librustc_bls12_381.a ${install_dir}
+  rm -rf ${BUILD_DIR}
 }
 
 
--- src/config/discover.ml
+++
@@ -1,5 +0,0 @@
-module C = Configurator.V1
-
-let () =
-  let libdir = "-L" ^ Sys.getenv "OPAM_SWITCH_PREFIX" ^ "/lib/librustc-bls12-381" in
-  C.Flags.write_sexp "flags.sexp" ["-lpthread"; libdir; "-lrustc_bls12_381"]
