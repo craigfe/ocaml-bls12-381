e54026f1fa38ba2370cc3f7104d15360c9499c34
Danny Willems <be.danny.willems@gmail.com>
2020-09-15 14:49:44 +0200

    Example ready

--- test/js/lib.ml
+++ test/js/lib.ml
@@ -3,7 +3,7 @@ let () =
   let open Js_of_ocaml in
   let p : (Jsoo_lib.ESModule.t, Unsafe.any) Jsoo_lib.Promise.t =
     Jsoo_lib.Promise.of_any_js
-      (Unsafe.js_expr {| import ("@dannywillems/rustc-bls12-381") |})
+      (Unsafe.js_expr {| import("@dannywillems/rustc-bls12-381") |})
   in
   let on_resolved m =
     Firebug.console##log (Jsoo_lib.ESModule.to_any_js m) ;
@@ -12,8 +12,8 @@ let () =
     end) in
     let module Fr = Bls12_381_base.Fr.MakeFr (StubsFr) in
     let a = Fr.random () in
-    (* let b = Fr.random () in
-     * let sum = Fr.add a b in *)
-    Firebug.console##log (Fr.to_string a)
+    let b = Fr.random () in
+    let sum = Fr.add a b in
+    Firebug.console##log (Fr.to_string sum)
   in
   Jsoo_lib.Promise.then_bind ~on_resolved p
