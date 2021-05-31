01b6f9203e5cfa7482fdf23718042e1dcf39b42d
Craig Ferguson <me@craigfe.io>
2021-05-31 13:15:07 +0100

    .

--- test/js/browser/run.js
+++
@@ -1,8 +0,0 @@
-import('@dannywillems/rustc-bls12-381').then((m) => {
-  window._RUSTC_BLS12_381 = m;
-  import('./test.js').then((m) => {
-    m.run();
-  }).catch((e) => {
-    console.log('Error while importing rustc-bls12-381 dep: ' + e);
-  });
-});
--- test/js/node/run.js
+++
@@ -1,3 +0,0 @@
-global._RUSTC_BLS12_381 = require('@dannywillems/rustc-bls12-381-node');
-const m = require('./test.js');
-m.run();
